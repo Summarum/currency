8ecf61e59eaa2539d9208da6b7fb4aa6d28838e5
Summarum <fubu10@o2.pl>
2017-03-20 12:57:17 +0100

    refactoring

--- mo.py
+++ mo.py
@@ -2,10 +2,10 @@ import sys
 import json
 from urllib2 import urlopen
 
-kittens = urlopen('http://api.fixer.io/latest?base='+sys.argv[1])
-d = kittens.read()
-response=json.loads(d)
-value = float(response['rates'][sys.argv[2]]) * float(sys.argv[3])
+apiURL = urlopen('http://api.fixer.io/latest?base='+sys.argv[1])
+rawJSON = apiURL.read()
+convertedJSON=json.loads(rawJSON)
+value = float(convertedJSON['rates'][sys.argv[2]]) * float(sys.argv[3])
 
 
 print value
